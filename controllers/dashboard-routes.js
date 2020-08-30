78665b9292be659501eed8d27107c33bdbbb991b
Alexsis Conley Brown <alexsismcb@gmail.com>
2020-08-30 02:14:35 -0500

    tweek

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -45,7 +45,7 @@ router.get('/', withAuth, (req, res) => {
 router.get('/edit/:id', withAuth, (req, res) => {
     Post.findOne({
         where: {
-            user_id: req.session.user_id
+            id: req.params.id
         },
         attributes: [
             'id', 
